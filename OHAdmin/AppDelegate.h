4fb15ea088dad302e979084188c885589c40e5cd
Vincent Do <do.vincent@live.com>
2015-11-28 10:04:06 -0500

    Initial Commit

---
+++ OHAdmin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OHAdmin
+//
+//  Created by Vincent Do on 11/28/15.
+//  Copyright © 2015 Vincent Do. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OHAdmin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OHAdmin
+//
+//  Created by Vincent Do on 11/28/15.
+//  Copyright © 2015 Vincent Do. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
